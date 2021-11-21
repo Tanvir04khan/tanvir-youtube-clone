1cf42b35aae5aca654ba5b43b8c71c201ef7da98
Tanvir04Khan <khantanvirrr98@gmail.com>
2021-11-21 14:46:43 +0530

    completed main content feature

--- src/components/Home/categoriesBar/CategoriesBar.jsx
+++ src/components/Home/categoriesBar/CategoriesBar.jsx
@@ -1,8 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import "./_categoriesBar.scss";
 
+const keywords = [
+  "All",
+  "React js",
+  "Angular js",
+  "React Native",
+  "use of API",
+  "Redux",
+  "Music",
+  "Algorithm Art ",
+  "Guitar",
+  "Bengali Songs",
+  "Coding",
+  "Cricket",
+  "Football",
+  "Real Madrid",
+  "Gatsby",
+  "Poor Coder",
+  "Shwetabh",
+];
+
 const CategoriesBar = () => {
-  return <div>categoriesbar</div>;
+  const [activeElement, setActiveElement] = useState("All");
+
+  const spanClickHandler = (value) => {
+    setActiveElement(value);
+  };
+
+  return (
+    <div className="categoriesbar">
+      {keywords.map((keyword, ind) => (
+        <span
+          kay={ind}
+          onClick={spanClickHandler.bind(null, keyword)}
+          className={activeElement === keyword ? "active" : ""}
+        >
+          {keyword}
+        </span>
+      ))}
+    </div>
+  );
 };
 
 export default CategoriesBar;
