890f9bf617a915e6284b49b5fab745f04d4689c5
Tanvir04Khan <khantanvirrr98@gmail.com>
2021-11-21 16:12:07 +0530

    completed Login page

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,17 @@ import { Container } from "react-bootstrap";
 import Header from "./components/Home/header/Header";
 import HomeScreen from "./components/Home/HomeScreen";
 import SideBar from "./components/Home/sideBar/SideBar";
+import LoginScreen from "./components/Login/LoginScreen";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 
 import "./_app.scss";
 
-const App = () => {
+const HomeScreenLayout = ({ children }) => {
   const [isSidebarVisible, setIsSidebarVisible] = useState(false);
 
   const handleToggleSidebar = () => setIsSidebarVisible((value) => !value);
@@ -20,10 +27,33 @@ const App = () => {
           onToggleSidebar={handleToggleSidebar}
         />
         <Container fluid className="app__main">
-          <HomeScreen />
+          {children}
         </Container>
       </div>
     </>
+  );
+};
+
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <HomeScreenLayout>
+            <HomeScreen />
+          </HomeScreenLayout>
+        </Route>
+        <Route path="/login">
+          <LoginScreen />
+        </Route>
+        <Route path="/search">
+          <h1>Search me</h1>
+        </Route>
+        <Route>
+          <Redirect to="/" />
+        </Route>
+      </Switch>
+    </Router>
   );
 };
 
